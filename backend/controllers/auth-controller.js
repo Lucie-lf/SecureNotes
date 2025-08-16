5a5f1510fdff8994d36f0284016780184e54989f
LUCI!!!!!! <lucie.le-foll@epitech.eu>
2025-08-16 15:01:14 +0200

    Token de verification set up, tests des mails de verification & de confirmation fonctionnels

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -2,6 +2,7 @@ import { User } from '../models/user.js';
 import bcrypt from 'bcryptjs';
 import { generateVerificationToken } from '../utils/generateVerificationToken.js';
 import { generateJWTToken } from '../utils/generateJWTToken.js';
+import { sendVerificationEmail, sendWelcomeEmail } from '../resend/email.js';
 
 export const signup = async (req, res) => {
     const { name, email, password } = req.body;
@@ -28,6 +29,8 @@ export const signup = async (req, res) => {
 
         generateJWTToken(res, user._id);
 
+        await sendVerificationEmail(user.email, verificationToken);
+
         res.status(201).json({
             success: true,
             message: 'User created successfully',
@@ -50,4 +53,40 @@ export const login = (req, res) => {
 
 export const logout = (req, res) => {
     res.send('Logout route');
+}
+
+export const verifyEmail = async (req, res) => {
+    const { code } = req.body;
+    try {
+        const user = await User.findOne({
+            verificationToken: code,
+            verificationExpiresAt: { $gt: Date.now() }, // Check if token is still valid
+        })
+        if (!user) {
+            return res
+            .status(400)
+            .json({ 
+                success: false,
+                message: 'Invalid or expired verification token' 
+            });
+        }
+        console.log("User found:", user);
+        // Mark user as verified
+        user.isVerified = true;
+        user.verificationToken = undefined; // Clear the token after verification
+        user.verificationExpiresAt = undefined; // Clear the expiration date
+        await user.save();
+
+        await sendWelcomeEmail(user.email, user.name);
+        res.status(200).json({
+            success: true,
+            message: 'Email verified successfully',
+        });
+    } catch (error) {
+        console.log("Error verifying email:", error);
+        res.status(400).json({ 
+            success: false,
+            message: 'Failed to verify email' 
+        });
+    }
 }
---
+++ backend/resend/email.js
@@ -0,0 +1,30 @@
+import { resend } from "./config.js";
+import { verificationTokenEmailTemplate, WELCOME_EMAIL_TEMPLATE } from "./email-templates.js";
+
+export const sendVerificationEmail = async (email, verificationToken) => {
+    try {
+        const { data, error } = await resend.emails.send({
+            from: "Acme <onboarding@resend.dev>",
+            to: [email],
+            subject: "Verification de votre compte",
+            html: verificationTokenEmailTemplate.replace("{verificationToken}", verificationToken),
+        });
+    } catch (error) {
+        console.log("Error sending email:", error);
+        throw new Error("Failed to send verification email");
+    }
+};
+
+export const sendWelcomeEmail = async (email, name) => {
+    try {
+        const { data, error } = await resend.emails.send({
+            from: "Acme <onboarding@resend.dev>",
+            to: [email],
+            subject: "Welcome to SecureNotes",
+            html: WELCOME_EMAIL_TEMPLATE.replace("{name}", name),
+        });
+    } catch (error) {
+        console.log("Error sending email:", error);
+        throw new Error("Failed to send verification email");
+    }
+}
